aa887cc45f2975b768cc7f636574d36becdd5eaa
heycalmdown <heyacct@gmail.com>
2017-01-18 15:56:41 +0900

    Change to use async-await for `plugin.ts`

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,6 +1,5 @@
 import * as mongoose from 'mongoose';
 import * as _debug from 'debug';
-import * as Bluebird from 'bluebird';
 import { Transaction } from './transaction';
 import { ObjectId } from './utils';
 
@@ -11,7 +10,134 @@ export interface TxDocument extends mongoose.Document {
   // __new?: boolean;
 }
 
-export function plugin(schema: mongoose.Schema, options?: Object) {
+class PreFindOne {
+  schema: any;
+
+  constructor (schema) {
+    this.schema = schema;
+  }
+
+  get options() {
+    return this.schema.options;
+  }
+
+  get model() {
+    return this.schema.model;
+  }
+
+  get _conditions() {
+    return this.schema._conditions;
+  }
+
+  get _fields() {
+    return this.schema._fields;
+  }
+
+  checkInSameTransaction(newTid, oldTid) {
+    if (!oldTid) return false;
+    if (newTid && newTid.equals(oldTid)) {
+      debug('already locked: ', oldTid);
+      return true;
+    }
+    return false;
+  }
+
+  isExpired(tid) {
+    const docTime = tid.getTimestamp();
+    const current = new Date().getTime();
+    debug('timestamp! ', docTime, current, current - docTime);
+
+    return (current - docTime) >= 30000;
+  }
+
+  async recommitTransaction(tModel, t) {
+    debug('recommit transaction');
+    await Transaction.recommit(t);
+    await tModel.update({_id: t._id}, {state: 'committed'}, {w: 1}).exec();
+  }
+
+  cancelTransaction(tModel, tid) {
+    debug('cancel transaction');
+    tModel.update({_id: tid}, {state: 'canceled'}, {w: 1}).exec();
+  }
+
+  async resolvePreviousTransaction(tid) {
+    const tModel = this.options.tModel;
+    debug('tModel is ', tModel);
+
+    if (!this.isExpired(tid)) return; // let it be conflicted
+
+    const transaction = await tModel.findOne({_id: tid}).exec();
+    if (!transaction) throw new Error('There is no transaction history.');
+
+    debug('find Transaction ', transaction, transaction.state);
+    switch (transaction.state) {
+      case 'init':
+        this.cancelTransaction(tModel, tid);
+        break;
+
+      case 'pending':
+        await this.recommitTransaction(tModel, transaction);
+        this._conditions['__t'] = {'$exists': false};
+        break;
+
+      case 'committed':
+        debug('already committed. ignore __t');
+        break;
+    }
+  }
+
+  isUpdateSuccessfully(rawResponse) {
+    return rawResponse.n > 0 && rawResponse.nModified > 0 && rawResponse.n === rawResponse.nModified;
+  }
+
+  async update(query) {
+    const rawResponse = await this.model.update(this._conditions, query, {
+      force: true,
+      w: 1
+    }).exec();
+    debug('rawResponse: %o', rawResponse);
+    if (!this.isUpdateSuccessfully(rawResponse)) {
+      throw new Error('write lock');
+    }
+  }
+
+  async getMinimalDoc(conditions) {
+    return this.model.findOne(conditions, {_id: 1, __t: 1}, {force: true}).exec();
+  }
+
+  async run() {
+    if (this.options.force) return;
+
+    debug('pre-findOne');
+    debug('options: %o', this.options);
+    debug('conditions: %o', this._conditions);
+
+    const doc = await this.getMinimalDoc(this._conditions);
+    if (!doc) return;
+    debug('document found! t : ', doc.__t, ', id : ', doc.id);
+
+    if (this.checkInSameTransaction(this.options.__t, doc.__t)) return;
+
+    if (doc.__t) {
+      await this.resolvePreviousTransaction(doc.__t);
+    }
+
+    this._conditions['_id'] = doc._id;
+    debug('conditions are modified', this._conditions);
+
+    const query = {__t: this.options.__t || ObjectId.get(Date.now())};
+    debug('update query is modified %o', query);
+    await this.update(query);
+
+    debug('locking success');
+    delete this._conditions['__t'];
+    debug('rollback conditions', this._conditions);
+    if (this._fields) this._fields['__t'] = 1;
+  }
+}
+
+export async function plugin(schema: mongoose.Schema, options?: Object) {
   schema.add({
     __t: { type: mongoose.Schema.Types.ObjectId },
     // __new: { type: Boolean, default: true }
@@ -28,83 +154,13 @@ export function plugin(schema: mongoose.Schema, options?: Object) {
     next();
   });
 
-  schema.pre('findOne', function(next) {
-    if (this.options.force) return next();
-    debug('pre-findOne');
-    debug('options: %o', this.options);
-
-    debug('conditions: %o', this._conditions);
-
-    return Promise.resolve(this.model.findOne(this._conditions, {_id: 1, __t: 1}, {force: true}).exec())
-      .then(doc => {
-        if (!doc) return next();
-        let tDisplace = false;
-        debug('document found! t : ', doc.__t, ', id : ', doc.id);
-
-        return Bluebird.try(() => {
-            if (doc.__t) {
-              // multiple findOne in a single transaction.
-              if (this.options.__t && this.options.__t.equals(doc.__t)) {
-                debug('already locked: ', doc.__t);
-                return next();
-              }
-
-              const tModel = this.options.tModel;
-              debug('tModel is ', tModel);
-              const docTime = doc.__t.getTimestamp();
-              const current = new Date().getTime();
-              debug('timestamp! ', docTime, current, current - docTime);
-
-              if (current - docTime >= 30000) {
-                // find Transaction
-
-                return Promise.resolve(tModel.findOne({_id: doc.__t}).exec())
-                  .then((t: any) => {
-                    debug('find Transaction ', t, t.state);
-                    if (!t) return next(new Error('There is no transaction history.'));
-                    switch (t.state) {
-                      case 'init':
-                        // cancel
-                        debug('cancel transaction');
-                        tDisplace = true;
-                        tModel.update({_id: doc.__t}, {state: 'canceled'}, {w: 1}).exec();
-                        break;
-                      case 'pending':
-                        // recommit
-                        debug('recommit transaction');
-                        return Transaction.recommit(t)
-                          .then(() => tModel.update({_id: doc.__t}, {state: 'committed'}, {w: 1}).exec());
-                      case 'committed':
-                        debug('already committed. ignore __t');
-                        tDisplace = true;
-                        break;
-                    }
-                  });
-              }
-            }
-          })
-          .then(() => {
-            this._conditions['_id'] = doc._id;
-            if (!tDisplace) this._conditions['__t'] = {'$exists': false};
-            debug('conditions are modified', this._conditions);
-            const update = {__t: this.options.__t || ObjectId.get(Date.now())};
-            debug('update query is modified %o', update);
-
-            return Promise.resolve(this.model.update(this._conditions, update, {
-              force: true,
-              w: 1
-            }).exec()).then((rawResponse) => {
-              debug('rawResponse: %o', rawResponse);
-              if (rawResponse.n > 0 && rawResponse.nModified > 0 && rawResponse.n === rawResponse.nModified) {
-                debug('locking success');
-                delete this._conditions['__t'];
-                debug('rollback conditions', this._conditions);
-                if (this._fields) this._fields['__t'] = 1;
-                return next();
-              }
-              return next(new Error('write lock'));
-            });
-          }).catch(next);
-      });
+  schema.pre('findOne', async function (next) {
+    const o = new PreFindOne(this);
+    try {
+      await o.run();
+      next();
+    } catch (e) {
+      next(e);
+    }
   });
 }
